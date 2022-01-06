728fd32d93af0b079a13f906e88bbe8a711c3278
Daniel Alejandro Hernández Llerena <dhdezllerena94@gmail.com>
2022-01-06 11:01:06 +0000

    feat: initial db conection settings with mongoose

--- project/src/index.ts
+++ project/src/index.ts
@@ -1,10 +1,16 @@
 import express from "express";
 import {json } from "body-parser";
 import { baseRouter } from "./routes/base.routes";
+import mongoose from "mongoose";
 
 const app = express();
 app.use(json());
 app.use(baseRouter);
+
+mongoose.connect("mongodb+srv://challengeUser:WUMglwNBaydH8Yvu@challenge-xzwqd.mongodb.net/getir-case-study?retryWrites=true",
+()=> {
+    console.info("Connected to Getir database.")
+})
 
 const PORT = 3000;
 
