5c1986efff913f885501c6fc314d47b85852ddc3
Daniel Alejandro Hernández Llerena <dhdezllerena94@gmail.com>
2022-01-06 22:12:49 +0000

    feature: base-rud (#1)
    * feat: base-get
    
    * feat: initial db conection settings with mongoose
    
    * feat: base model
    
    * feat: post base
    
    * feat: dotenv
    
    * feat: swagger
    
    * chore: vscode config folder
    
    * feat: docker and eslint

--- project/src/index.ts
+++ project/src/index.ts
@@ -1,11 +1,49 @@
-import express from "express";
-import {json } from "body-parser";
+require('dotenv').config();
+import express from 'express';
+import {json} from 'body-parser';
+import {baseRouter} from './routes/base.routes';
+import mongoose from 'mongoose';
+const swaggerUi = require('swagger-ui-express');
+const basicAuth = require('express-basic-auth');
 
-const app = express()
-app.use(json())
+const port = process.env.NODE_PORT || 3000;
 
-const PORT = 3000;
+const app = express();
+app.use(json());
 
-app.listen(PORT, () => {
-    console.info(`Server is listening on port: ${PORT}.`)
-})
+const swaggerJsdoc = require('swagger-jsdoc');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Base Project',
+      version: '1.0.0',
+    },
+  },
+  apis: ['./src/routes/**.routes.ts'],
+};
+
+const swaggerPassword = process.env.SWG_PASSWORD;
+
+const swaggerSpec = swaggerJsdoc(options);
+app.use(
+  '/api-docs',
+  basicAuth({
+    users: {users: swaggerPassword},
+    challenge: true,
+  }),
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerSpec)
+);
+
+app.use(baseRouter);
+
+mongoose.connect(process.env.DB_HOST ? process.env.DB_HOST : '', err => {
+  if (err) throw err;
+  console.info('Connected to DB.');
+});
+
+app.listen(port, () => {
+  console.info(`Server is listening on port: ${port}.`);
+});
---
+++ project/src/routes/base.routes.ts
@@ -0,0 +1,46 @@
+import express, {Request, Response} from 'express';
+import {Base, IBase} from '../models/base.model';
+
+const router = express.Router();
+
+/**
+ * @openapi
+ * /api/base:
+ *   get:
+ *     description: Get "The base.".
+ *     responses:
+ *       200:
+ *         description: returns string.
+ */
+router.get('/api/base', [], (req: Request, res: Response) => {
+  return res.send('The base.');
+});
+
+/**
+ * @openapi
+ * /api/base:
+ *   post:
+ *     description: Create a register.
+ *     responses:
+ *       201:
+ *         description: Returns the registry.
+ */
+router.post('/api/base', async (req: Request, res: Response) => {
+  const baseDto: IBase = req.body;
+  const base = new Base({
+    startDate: baseDto.startDate,
+    endDate: baseDto.endDate,
+    minCount: baseDto.minCount,
+    maxCount: baseDto.maxCount,
+  });
+
+  try {
+    await base.save();
+  } catch (error) {
+    console.error(error);
+  }
+
+  return res.status(201).send(base);
+});
+
+export {router as baseRouter};
